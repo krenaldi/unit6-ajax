2c3b317856a08b8b5dd680df1b30884c23608fd0
Kris Renaldi <krenaldi@comcast.net>
2019-07-09 14:02:11 -0400

    Added displayGifs function and click event to trigger ajax call to giphy to trigger displayGifs

--- javascript/logic.js
+++ javascript/logic.js
@@ -19,7 +19,36 @@ function renderButtons() {
 }
 
 // Function that renders the HTML to display gifs from Giphy
+function displayGifs(){
+    var topic = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
+    //Creating AJAX call for specific button clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        // Since image info is inside the data key, using variable to hold full key
+        var results = response.data;
+        // Looping thru response data and generate divs to hold images & ratings info
+        for (var i=0; i < results.length; i++){
+            var gifDiv = $("<div>");
+            var p = $("<p>").text(results[i].rating);
+            var gifImage = $("<img>");
+            gifImage.addClass("gif");
+            gifImage.attr({
+                src: results[i].images.fixed_height.url,
+                "data-still": results[i].images.fixed_height.url,
+                "data-animate": results[i].images.fixed_height.url,
+                "data-state": "still"
+            });
+            gifDiv.append(gifImage);
+            gifDiv.append(p);
+            $("#gifs-appear-here").prepend(gifDiv);
+        }
+    })
+}
 
 
 // Event handler to add button from form to the button-div thru the topics array
@@ -36,3 +65,6 @@ $("#add-gif-button").on("click", function(event) {
 
 // Calling renderButtons function to display the initial buttons in the topics array
 renderButtons();
+
+// Adding click event for all button elements with class of gif to trigger the displayGifs function
+$("button").on("click", ".gif", displayGifs);
